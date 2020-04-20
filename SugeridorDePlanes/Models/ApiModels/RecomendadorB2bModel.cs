25843f25703ace1e229c156ac1e15a4230e1d9d8
HernanPerez7 <hernan-perez96@hotmail.com>
2020-04-20 14:53:04 -0300

    mostrar datos planes en la vista

--- SugeridorDePlanes/Models/ApiModels/RecomendadorB2bModel.cs
+++ SugeridorDePlanes/Models/ApiModels/RecomendadorB2bModel.cs
@@ -13,6 +13,27 @@ namespace Telefonica.SugeridorDePlanes.Models.ApiModels
         public int? Movil { get; set; }
         public string CodPlan { get; set; }
         public decimal? Tmm { get; set; }
-        public decimal? TmmSinIva { get; set; }
+        public int? CantidadPasaportes { get; set; }
+        public string Prestacion1 { get; set; }
+        public decimal? PrecioPrestacion1SinIva { get; set; }
+        public decimal? IvaPrestacion1 { get; set; }
+        public string Prestacion2 { get; set; }
+        public decimal? PrecioPrestacion2SinIva { get; set; }
+        public decimal? IvaPrestacion2 { get; set; }
+        public decimal? TmmPrestacionSinIva { get; set; }
+        public decimal? TmmPrestacionIvaInc { get; set; }
+        public decimal? TmmPrestacion { get; set; }
+        public decimal? MbTotal { get; set; }
+        public decimal? MbTotalProm { get; set; }
+        public string Roamer { get; set; }
+        public string RoamerProm { get; set; }
+        public string PlanSugerido { get; set; }
+        public string PlanSugeridoRetencion { get; set; }
+        public int? BonoPlanSugerido { get; set; }
+        public decimal? TmmPlanSugerido { get; set; }
+        public int? BonoPlanRetencion { get; set; }
+        public decimal? TmmPlanRetencion { get; set; }
+        public string RoamingPlanSugerido { get; set; }
+        public string RoamingPlanRetencion { get; set; }
     }
 }
