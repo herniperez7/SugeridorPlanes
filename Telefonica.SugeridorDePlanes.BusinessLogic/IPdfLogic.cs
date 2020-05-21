3e5f5e818fc3dbbaf9f4c5e9ff0835a236a1156b
gjulean <gjulean1991@gmail.com>
2020-05-21 16:16:35 -0300

    clase de logica

---
+++ Telefonica.SugeridorDePlanes.BusinessLogic/IPdfLogic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Telefonica.SugeridorDePlanes.BusinessLogic
+{
+    public interface IPdfLogic
+    {
+        void GeneratePdfFromHtml();
+
+    }
+}
---
+++ Telefonica.SugeridorDePlanes.BusinessLogic/PdfLogic.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Telefonica.SugeridorDePlanes.BusinessLogic
+{
+   public  class PdfLogic: IPdfLogic
+    {
+
+        public void GeneratePdfFromHtml()
+        {
+            string fileName = "HTML.html";
+            string tempName = Path.GetRandomFileName() + ".html";
+
+            // path donde se encuentra el HTML original
+            string sourcePath = @"C:\Users\Usuario\Desktop\Proyecto\pdf";
+            string tempDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            string sourceFile = Path.Combine(sourcePath, fileName);
+            string destFile = Path.Combine(tempDirectory, tempName);
+            Directory.CreateDirectory(tempDirectory);
+            System.IO.File.Copy(sourceFile, destFile, true);
+
+
+            ////logica para hacer reemplazar
+
+            StreamReader objReader = new StreamReader(destFile);
+            string content = objReader.ReadToEnd();
+            objReader.Close();
+            content = Regex.Replace(content, "{change}", "prueba union");
+
+            StreamWriter writer = new StreamWriter(destFile);
+            writer.Write(content);
+            writer.Close();
+
+
+        }
+        
+
+
+
+
+    }
+}
