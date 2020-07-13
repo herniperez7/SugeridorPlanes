ec572935f131f36227d7f181d045dc7dafc68d23
Gonzalo <gjulean@intermedia.com.uy>
2020-07-12 23:02:34 -0300

    fix altura pdf

--- SugeridorDePlanes/wwwroot/js/Sugeridor/SugeridorScripts.js
+++ SugeridorDePlanes/wwwroot/js/Sugeridor/SugeridorScripts.js
@@ -639,6 +639,7 @@ function saveByteArray(reportName, byte) {
     var link = document.createElement('a');
     link.href = window.URL.createObjectURL(blob);
     var fileName = reportName;
+  
     link.download = fileName;
     link.click();
 }
